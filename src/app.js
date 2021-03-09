36f21215e3dfb810fda3a99ef60bf97352d1b311
Amit Alon <amit@kaholo.io>
2021-03-09 19:54:11 +0200

    finished initial plugin

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const child_process = require("child_process")
+
+async function runCLICommand(command, vaultUrl){
+	return new Promise((resolve,reject) => {
+		const callback = (error, stdout, stderr) => {
+			if (error) {
+			   return reject(`exec error: ${error}`);
+			}
+			if (stderr) {
+				return resolve(stderr);
+			}
+			return resolve("success"); // not printing stdout, because -no-print=true flag doesn't work
+		};
+		if (vaultUrl){
+			child_process.exec(command, {env: {'VAULT_ADDR': vaultUrl}}, callback);
+		}
+		else {
+			child_process.exec(command, callback);
+		}
+	})
+}
+
+async function login(action, settings){
+	const token = action.params.token || settings.token;
+	if (!token){
+		throw "token was not provided";
+	}
+	const vaultUrl = (settings.vaultUrl || "").trim();
+
+	const loginCmd = `vault login ${token}`;
+	return runCLICommand(loginCmd, vaultUrl);
+}
+
+module.exports = {
+    login
+}
